582fc9a14f5adf3ed28b47103e5a61212da0b13b
Baihaki Kuncara <baihakidwi@gmail.com>
2023-07-07 21:24:26 +0700

    -adjust ball's angle after bouncing on board

--- Scenes/Ball.gd
+++ Scenes/Ball.gd
@@ -9,8 +9,6 @@ export var speed: int = 200
 export var direction: Vector2	= Vector2(1,1)
 
 
-
-
 func _draw():
 	draw_circle(Vector2(0,0), size, color)
 
@@ -18,10 +16,15 @@ func _draw():
 func _physics_process(delta):
 	var collision = move_and_collide(direction *speed*delta)
 	if (collision):
-		direction = direction.bounce((collision.normal))
+		var modifier = 0
+		if collision.collider.has_method("collide"):
+			modifier = collision.collider.collide(collision.position)
+		var angle = collision.normal
+		direction = direction.bounce(angle).rotated(modifier).normalized()
 		speed = min(speed+SPEED_INCREASE, MAX_SPEED)
 
 
 func delete_node():
+	print("queue_free")
 	queue_free()
 	
--- Scenes/Board.gd
+++ Scenes/Board.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const MAX_DEGREE_MODIFIER = 0.5
+
 export var speed: float = 800
 
 var direction: Vector2 = Vector2(0,0)
@@ -10,5 +12,11 @@ func _process(delta):
 		direction.x -= 1
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
-
 	move_and_collide(direction * speed * delta)
+
+
+func collide(var p:Vector2):
+	var shape:CollisionShape2D = get_node("CollisionShape2D")
+	var size = shape.shape.extents.x
+	return (p.x - position.x)/size * MAX_DEGREE_MODIFIER
+	
