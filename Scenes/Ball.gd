dcca78d23ffca5009ca4a85d2e960136fdce6bff
Baihaki Kuncara <baihakidwi@gmail.com>
2023-07-08 14:39:38 +0700

    -separate drawing from Ball node

--- Scenes/Ball.gd
+++ Scenes/Ball.gd
@@ -3,14 +3,8 @@ extends KinematicBody2D
 const MAX_SPEED = 1000
 const SPEED_INCREASE = 10
 
-export var color: Color = Color.azure
-export var size: float = 5.0
 export var speed: int = 200
-export var direction: Vector2	= Vector2(1,1)
-
-
-func _draw():
-	draw_circle(Vector2(0,0), size, color)
+export var direction: Vector2	= Vector2(0,-1)
 
 
 func _physics_process(delta):
---
+++ Scenes/BallSprite.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+export var color: Color = Color.azure
+export var size: float = 5.0
+
+
+func _draw():
+	draw_circle(Vector2(0,0), size, color)
