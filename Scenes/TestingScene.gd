49e7ad842d89f1a0dc084fa48ec753c1634f87c8
Baihaki Kuncara <baihakidwi@gmail.com>
2023-07-18 14:22:04 +0700

    -adjust testing scene

--- Scenes/TestingScene.gd
+++ Scenes/TestingScene.gd
@@ -2,11 +2,9 @@ extends Node2D
 
 
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
-		$Board.resume()
-	if Input.is_action_pressed("ui_page_up"):
+	if Input.is_action_just_released("ui_page_up"):
 		print("resize+")
-		$Board.resize(1)
-	if Input.is_action_pressed("ui_page_down"):
+		$Player/Board.resize(1)
+	if Input.is_action_just_released("ui_page_down"):
 		print("resize-")
-		$Board.resize(-1)
+		$Player/Board.resize(-1)
