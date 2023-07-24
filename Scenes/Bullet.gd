aa23aa900a4b48e56cf035a600c67ee49dec4007
baihaki.kuncara <baihakidwi@gmail.com>
2023-07-24 14:52:22 +0700

    -create bullet

---
+++ Scenes/Bullet.gd
@@ -0,0 +1,34 @@
+
+extends KinematicBody2D
+
+var speed = 10
+var pause = true
+
+
+func _draw():
+	draw_rect(Rect2(-$CollisionShape2D.shape.extents, $CollisionShape2D.shape.extents*2), Color.red)
+
+
+func _process(delta):
+	if pause:
+		return
+	var collision = move_and_collide(Vector2(0, -1) * speed)
+	if collision:
+		if collision.collider.has_method("hit"):
+			collision.collider.hit()
+		queue_free()
+
+
+func pause():
+	pause = true
+	for children in get_children():
+		if children.has_method("pause"):
+			children.pause()
+			
+			
+func resume():
+	pause = false
+	for children in get_children():
+		if children.has_method("resume"):
+			children.resume()
+	
